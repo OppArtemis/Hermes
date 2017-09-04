1a06572e0a5843807ae5b96dbdb17b0b0cd5dc94
j2quan <koino.warrior@gmail.com>
2017-09-04 02:51:51 -0400

    more revamp

--- Hermes/app/src/main/java/com/artemis/hermes/android/MainActivity.java
+++ Hermes/app/src/main/java/com/artemis/hermes/android/MainActivity.java
@@ -1,5 +1,11 @@
 package com.artemis.hermes.android;
 
+
+/**
+ * Main Activity, the entry point for the app, which
+ * requires users to login.
+ */
+
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,107 +13,96 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Button;
 import android.widget.Toast;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import java.util.Arrays;
 
 // Firebase UI libraries
 import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.AuthUI.IdpConfig;
 import com.firebase.ui.auth.IdpResponse;
 import com.firebase.ui.auth.ErrorCodes;
 
 // General Firebase libraries
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
-
-
-// General libraries
-import android.content.Intent;
-import java.util.Arrays;
-
-// Debugging
-import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button loginButton;
+    private FirebaseAuth auth;
+
     // Choose an arbitrary request code value
-    private static final int RC_SIGN_IN = 123;
+    private static final int RC_SIGN_IN = 128;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        FirebaseAuth auth = FirebaseAuth.getInstance();
+        auth = FirebaseAuth.getInstance();
         FirebaseUser user = auth.getCurrentUser();
         if (user == null) {
 
-            startActivityForResult(
-                    AuthUI.getInstance()
-                            .createSignInIntentBuilder()
-                            .setIsSmartLockEnabled(!BuildConfig.DEBUG)
-                            .setAvailableProviders(
-                                    Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
-                            .build(),
-                    RC_SIGN_IN);
-            finish();
-            return;
+            setContentView(R.layout.activity_main);
+
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            loginButton = (Button)findViewById(R.id.login_button);
+            loginButton.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View view) {
+                   startActivityForResult(AuthUI.getInstance()
+                           .createSignInIntentBuilder()
+                           .setIsSmartLockEnabled(!BuildConfig.DEBUG)
+                           .setAvailableProviders(
+                                   Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build(),
+                                                 new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build(),
+                                                 new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build()))
+                           .build(),
+                           RC_SIGN_IN);
+               }
+            });
        } else {
-            // already signed in, do nothing
-            String userName = user.getDisplayName();
-            String myEmail = user.getEmail();
-
-            Log.d("My name", userName);
-            Log.d("my email", myEmail);
-
-            setContentView(R.layout.login_info);
-            TextView textView1 = (TextView) this.findViewById(R.id.userName);
-            textView1.setText(String.valueOf(userName));
-
-            TextView textView2 = (TextView) this.findViewById(R.id.myEmail);
-            textView2.setText(String.valueOf(myEmail));
+            loginUser();
         }
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        // Set up my return variable
-        String result = "";
-
         // RC_SIGN_IN is the request code you passed into startActivityForResult(...) when starting the sign in flow.
         if (requestCode == RC_SIGN_IN) {
-            IdpResponse response = IdpResponse.fromResultIntent(data);
 
-            // Successfully signed in
-            if (resultCode == RESULT_OK) {
-                result="OK";
+            if(resultCode == RESULT_OK){
+                loginUser();
             } else {
-                // Sign in failed
-                if (response == null) {
-                    result="Null";
+
+                IdpResponse response = IdpResponse.fromResultIntent(data);
+                if (resultCode == RESULT_CANCELED) {
+                    displayMessage(getString(R.string.signin_failed));
                 }
 
                 if (response.getErrorCode() == ErrorCodes.NO_NETWORK) {
-                    result="No Network";
-                }
-
-                if (response.getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
-                    result="Unknown Error";
+                    displayMessage(getString(R.string.no_network));
                 }
             }
 
-            result="Internal Error";
-
-            Log.d("the result", result);
-
-            setContentView(R.layout.activity_main);
-            TextView textView = (TextView) this.findViewById(R.id.signInResult);
-            textView.setText(String.valueOf(result));
+            return;
         }
+        displayMessage(getString(R.string.unknown_response));
+    }
+
+    private void loginUser(){
+        Intent loginIntent = new Intent(MainActivity.this, SigninActivity.class);
+        startActivity(loginIntent);
+        finish();
+    }
+
+    private void displayMessage(String message){
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
     }
 
 //    @Override
